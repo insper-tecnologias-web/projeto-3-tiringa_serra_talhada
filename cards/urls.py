9236aedeeeba757d27b74a2f699d8261ae4a9aa6
Ricardo Mourão <ricardomrf@al.insper.edu.br>
2022-05-31 15:58:34 -0300

    Fronted completo

--- cards/urls.py
+++ cards/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('imoveis.html', views.imoveis, name='imoveis'),
     path('update/<int:id>', views.edita, name='update'),
     path('delete/<int:id>', views.delete, name='delete'),
+    path('bairroSelecionado', views.bairroSelecionado, name='bairroSelecionado')
 ]
